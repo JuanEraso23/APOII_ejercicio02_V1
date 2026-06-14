497258051cd595cf4dc1cda9e5c89561221256eb
Juan Manuel Eraso Grijalba <144852394+JuanEraso23@users.noreply.github.com>
2026-06-14 16:07:50 -0500

    Update Main.java

--- src/context/Main.java
+++ src/context/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Atributos
-		float n1=5;
-	    float n2=2;
-	    float n3=9;
+		float n1 = 5;
+	    float n2 = 2;
+	    float n3 = 9;
 	    
 	    //Formula
-	    float prom=((n1+n2+n3)/3);
+	    float prom=((n1 + n2 + n3) / 3);
 	    
 	    //Resultado
-	    System.out.print("Promedio: "+prom);
+	    System.out.print("Promedio: " + prom);
 	}
 
 }
